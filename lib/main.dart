1e57dc1bcd8aa257c6b6efb4b4977bf29482728f
insanansharyrasul <insanansryrasul21@gmail.com>
2025-03-03 16:53:15 +0700

    feat - added code for the app to be working

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,62 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int _randomNumber = 0;
+  Random random = Random();
+
+  void generateRandomNumber() {
+    setState(() {
+      _randomNumber = random.nextInt(5) + 1;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData.dark(),
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: Container(
+                    height: 150,
+                    width: 150,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                    ),
+                    child: Image.asset('assets/$_randomNumber.png')),
+              ),
+              const SizedBox(height: 20), 
+              ElevatedButton(
+                onPressed: generateRandomNumber,
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(Colors.amber),
+                ),
+                child: const Text(
+                  'Generate Random Number',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
